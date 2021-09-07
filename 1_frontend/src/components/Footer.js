21951e7b5a00acc16b5c7e80378c6e0021a23173
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-07 13:26:04 +0300

    created footer

---
+++ 1_frontend/src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+  return <footer>&copy; {new Date().getFullYear()} All rights reserved</footer>;
+};
+
+export default Footer;
