8dc0c982a55cba52114b7a81e0432926bb0a5d45
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-07 13:27:40 +0300

    created account starter

---
+++ 1_frontend/src/screens/MyAccountScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MyAccountScreen = () => {
+  return (
+    <main>
+      <section>
+        <h2>My Account</h2>
+      </section>
+    </main>
+  );
+};
+
+export default MyAccountScreen;
