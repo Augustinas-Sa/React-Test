4e9c3b1ff8ef1f5821d244a6f53b4ccfeb1d4df1
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-07 13:25:47 +0300

    created content

--- 1_frontend/src/App.js
+++ 1_frontend/src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// screens
+import HomeScreen from './screens/HomeScreen';
+import MyAccountScreen from './screens/MyAccountScreen';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomeScreen} />
+        <Route path='/my-account' component={MyAccountScreen} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
