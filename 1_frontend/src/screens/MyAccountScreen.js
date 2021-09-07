92444f7b69e04dc930672623b3bbc4bfa52d9da0
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-07 19:59:58 +0300

    created account screen logic

--- 1_frontend/src/screens/MyAccountScreen.js
+++ 1_frontend/src/screens/MyAccountScreen.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './MyAccountScreen.css';
+import Counter from '../components/Counter';
 
 const MyAccountScreen = () => {
+  // hooks
+  // - local state
+  const [teams, setTeams] = useState([]);
+
+  // - side effects
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/teams').then((response) => {
+      setTeams(response.data);
+    });
+  }, []);
+
   return (
     <main>
       <section>
-        <h2>My Account</h2>
+        <h2 className='teams-name'>All teams</h2>
+        <div className='main-teams-div'>
+          {teams.map((item) => (
+            <div className='card_shadow' key={item._id}>
+              <img className='images' src={item.image} alt='' />
+              <Counter />
+            </div>
+          ))}
+        </div>
       </section>
     </main>
   );
