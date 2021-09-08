90fae17d9ae79e2f945c104c846d633736d6eca0
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-08 12:25:20 +0300

    added more logic

--- 1_frontend/src/components/Header.js
+++ 1_frontend/src/components/Header.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import { FaUsers } from 'react-icons/fa';
+import { UserContext } from '../App';
 
 const Header = () => {
+  // hooks
+  // - state
+  const { state, dispatch } = useContext(UserContext);
+
+  // - side effects
+  useEffect(() => {
+    if (localStorage.getItem('user')) {
+      console.log('user founded');
+
+      dispatch({
+        type: 'LOGIN',
+        payload: localStorage.getItem('user'),
+      });
+    } else {
+      console.log('user not founded');
+    }
+  }, [dispatch]);
+
   return (
     <header>
       <div>
@@ -16,11 +35,17 @@ const Header = () => {
               Home
             </Link>
           </li>
-          <li>
-            <Link to='/my-account' className='account-link'>
-              My Account
-            </Link>
-          </li>
+          {state.user ? (
+            <li>
+              <Link to='/my-account' className='account-link'>
+                My Account
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link to='/' className='home-link'></Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
