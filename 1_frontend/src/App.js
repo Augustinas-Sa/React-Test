e0a99093c595d8ea96613d8e54b36b2b9d43f31d
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-08 10:12:53 +0300

    added more logic

--- 1_frontend/src/App.js
+++ 1_frontend/src/App.js
@@ -1,23 +1,52 @@
 import './App.css';
-import React from 'react';
+import React, { useReducer } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // screens
 import HomeScreen from './screens/HomeScreen';
 import MyAccountScreen from './screens/MyAccountScreen';
+
+// components
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+// context
+export const UserContext = React.createContext();
+
+// state management
+// - global
+const initialState = { user: '' };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'REGISTER':
+      return { user: action.payload };
+    case 'LOGIN':
+      return { user: action.payload };
+    case 'LOGOUT':
+      return { user: '' };
+    default:
+      return state;
+  }
+};
+
 function App() {
+  // hooks
+  // - state
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomeScreen} />
-        <Route path='/my-account' component={MyAccountScreen} />
-      </Switch>
-      <Footer />
-    </Router>
+    <>
+      <UserContext.Provider value={{ state, dispatch }}>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={HomeScreen} />
+            <Route path='/my-account' component={MyAccountScreen} />
+          </Switch>
+          <Footer />
+        </Router>
+      </UserContext.Provider>
+    </>
   );
 }
 
